4539ce24bff0ebeef1cc17c33b4cbc0abfe98e9c
hal <forarslys@gmail.com>
2020-04-02 06:59:56 +0900

    Implement option parse

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 pub mod snes;
 
 fn main() {
-	let args: Vec<_> = std::env::args().collect();
-	if args.len() < 2 {
-		return;
-	}
-
+	let matches = clap::App::new(clap::crate_name!())
+		.version(clap::crate_version!())
+		.author("forarslys")
+		.about("PNG to SNES graphics convertor")
+		.arg(clap::Arg::with_name("input").help("input PNG file").required(true))
+		.arg(clap::Arg::with_name("2bpp").help("Converts to 2bpp").long("2bpp").short("2"))
+		.arg(clap::Arg::with_name("4bpp").help("Converts to 4bpp").long("4bpp").short("4"))
+		.arg(clap::Arg::with_name("8bpp").help("Converts to 8bpp").long("8bpp").short("8"))
+		.group(clap::ArgGroup::with_name("bpp").args(&["2bpp", "4bpp", "8bpp"]))
+		.get_matches();
 }
